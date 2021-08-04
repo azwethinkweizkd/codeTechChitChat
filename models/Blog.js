fe6256b2dfa04f9bdcb1f5bb84701cdfd64868ba
Kevin Devlin <azwethinkweizkd@gmail.com>
2021-08-03 22:59:06 -0600

    Filled in model for Blog

--- models/Blog.js
+++ models/Blog.js
@@ -2,3 +2,31 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
 class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blog",
+  }
+);
+
+module.exports = Blog;
